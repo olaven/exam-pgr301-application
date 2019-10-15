1b5428441bc345abc3be2e0d8b9b1fc04594ed23
lagasild <lagasild@protonmail.com>
2019-10-15 10:49:23 +0200

    first pass at all required endpoints

--- src/main/kotlin/org/devops/exam/controller/DeviceController.kt
+++ src/main/kotlin/org/devops/exam/controller/DeviceController.kt
@@ -1,15 +1,18 @@
 package org.devops.exam.controller
 
 import org.devops.exam.entity.Device
+import org.devops.exam.entity.Measurement
 import org.devops.exam.repository.DeviceRepository
 import org.devops.exam.repository.MeasurementRepository
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.*
 import org.springframework.web.bind.annotation.*
+import sun.audio.AudioDevice.device
 import java.lang.Exception
 import java.net.URI
 import javax.validation.ConstraintViolationException
+import kotlin.system.measureNanoTime
 
 @RestController
 class DeviceController(
@@ -19,7 +22,7 @@ class DeviceController(
 
     @PostMapping("/devices", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun postDevice(
-            @RequestBody device: Device
+            @RequestBody device: Device //TODO: ask if this should take a device or not. If not, update!
     ): ResponseEntity<Device> {
 
         //i.e. user tries to decide ID
@@ -28,38 +31,43 @@ class DeviceController(
            return status(409).body(null)
         }
 
-        return try {
+        return handleConstraintViolation {
 
             val persisted = deviceRepository.save(device)
             created(URI.create("${persisted.deviceId}")).body(persisted) //TODO: add endpoint for getting _one_ device (if so, update location)
-        } catch (exception: Exception) {
-
-            if (exception is ConstraintViolationException) {
-
-                status(400).build()
-            } else {
-
-                badRequest().build()
-            }
         }
     }
 
-    @PostMapping(" /device/{deviceId}/measurements")
+    @PostMapping(" /devices/{deviceId}/measurements")
     fun postMeasurement(
-            @PathVariable deviceId: Long
-    ) {
-        
+            @PathVariable("deviceId") deviceId: Long,
+            @RequestBody measurement: Measurement
+    ): ResponseEntity<Measurement> {
 
+        if (measurement.id != null || deviceId != measurement.id) {
+            return status(409).build()
+        }
+
+        val entity = deviceRepository.findById(deviceId)
+        if (!entity.isPresent) return notFound().build()
+
+        return handleConstraintViolation {
+
+            val persisted = measurementRepository.save(measurement)
+            created(URI.create("/devices/$deviceId/measurements")).body(persisted)
+        }
     }
 
-    @GetMapping("/device/{id}/measurements")
-    fun getMeasurements() {
+    @GetMapping("/devices/{id}/measurements")
+    fun getMeasurements(
+            @PathVariable("id") id: Long
+    ): ResponseEntity<Iterable<Measurement>> {
 
+        if (!deviceRepository.existsById(id)) return notFound().build()
+        val measurements = measurementRepository.findByDeviceDeviceId(id)
+        return ok(measurements)
     }
 
     @GetMapping("/devices")
-    fun getDevices() {
-
-
-    }
+    fun getDevices() = ok(deviceRepository.findAll())
 }
