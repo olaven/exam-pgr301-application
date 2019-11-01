261e5075a30419048a5e1c8623cef22de723d9ee
lagasild <lagasild@protonmail.com>
2019-11-01 18:35:30 +0100

    not setting profile in code

--- src/main/kotlin/org/devops/exam/App.kt
+++ src/main/kotlin/org/devops/exam/App.kt
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(App::class.java, "--spring.profiles.active=local")
+    //SpringApplication.run(App::class.java, "--spring.profiles.active=local")
+    SpringApplication.run(App::class.java)
 }
 
 @SpringBootApplication
