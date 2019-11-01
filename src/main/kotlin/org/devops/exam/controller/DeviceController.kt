2d84b4ff5a86727c66e1244367f09d07a835bcad
olaven <olav@sundfoer.com>
2019-11-01 16:56:31 +0100

    logback differentiates between local and deployed

--- src/main/kotlin/org/devops/exam/controller/DeviceController.kt
+++ src/main/kotlin/org/devops/exam/controller/DeviceController.kt
@@ -9,13 +9,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.http.ResponseEntity.*
+import org.springframework.http.ResponseEntity.created
+import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
-import java.util.concurrent.TimeUnit
 
 @RestController
 class DeviceController(
